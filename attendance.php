6f0f0fb03f13b43a8b235ba5ade6ffbd66a36d5a
Itzrtune <rohitladdha26@gmail.com>
2022-02-25 16:08:09 +0530

    init

---
+++ attendance.php
@@ -0,0 +1,40 @@
+<!-- First skeleton made by Ashir for creating the main framework of the UI -->
+
+<!-- 
+    This is the templete 
+ -->
+
+
+
+<!-- PHP Session to verify if the user has logged in, if not redirect to log in page -->
+<?php
+    session_start();
+    error_reporting(0);
+    include('includes/config.php');
+    if(strlen($_SESSION['alogin'])==0)
+    {
+        header('location:index.php');
+    }
+    else
+    {
+        //main logic
+?>
+
+<!-- main logic -->
+<!doctype html>
+<html lang="en">
+    
+    <head>
+        <title>Attendance Details</title>
+        <link rel = "icon" href = "img/icon.png" type = "image/x-icon">
+    </head>
+
+    <body >
+    
+    </body>
+</html>
+
+<!-- PHP end here -->
+<?php
+    }
+?>
