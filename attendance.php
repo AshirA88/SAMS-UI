e48e9df13004a24e36e3308095434a53cb023db0
Manali Gadiya <56933487+manali-11@users.noreply.github.com>
2022-03-10 18:04:30 +0530

    Update attendance.php

--- attendance.php
+++ attendance.php
@@ -23,18 +23,76 @@
 <!-- main logic -->
 <!doctype html>
 <html lang="en">
-    
     <head>
         <title>Attendance Details</title>
         <link rel = "icon" href = "img/icon.png" type = "image/x-icon">
+        <link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css' />
+        <link href='http://fonts.googleapis.com/css?family=Abel|Satisfy' rel='stylesheet' type='text/css' />
+        <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
     </head>
 
-    <body >
-    
-    </body>
-</html>
+    <style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
 
-<!-- PHP end here -->
-<?php
-    }
-?>
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #D6EEEE;
+}
+</style>
+<body>
+<div id="header-wrapper">
+	    <div id="header">
+		    <div id="logo">
+        <h1><a href="javascript:history.back()"><-</a> 
+			    <a>Attendance details</a></h1>
+		    </div>
+      </div>
+  </div>
+    <div id="wrapper">
+	<div id="page-wrapper">
+		<div id="page">
+		   <div id="wide-content">
+<div id= "table">
+<table>
+  <tr>
+  <th>Sr. No </th>
+  <th> Subject</th>
+  <th>Attendance </th>
+  </tr>
+  <tr>
+  <td>1</td>
+  <td>Software Modeling</td>
+  <td>78%</td>
+  </tr>
+  <tr>
+  <td>2</td>
+  <td>Computer Automation</td>
+  <td>82%</td>
+  </tr>
+  <tr>
+  <td>3</td>
+  <td>UI/UX Designing</td>
+  <td>91%</td>
+  </tr>
+  <tr>
+  <td>4</td>
+  <td>Security and Privacy in Media</td>
+  <td>90%</td>
+  </tr>
+</table>
+</div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div id="footer" class="container">
+</div>
+</body>
+</html>
