6c5e054eeb9538ecee06964af53d4a29b4639957
Manali Gadiya <manali.gadiya11@gmail.com>
2022-03-28 22:01:12 +0530

    git init

--- ashir/admin/add_account.php
+++ ashir/admin/add_account.php
@@ -21,6 +21,25 @@
         <link rel = "icon" href = "../img/logo.png" type = "image/x-icon">
     </head>
 
+    <body>
+        
+    <h2>&emsp;Add New Account Details</h2>
+
+        <form action="#">&emsp;
+        <label for="fname">First name:</label><br>&emsp;
+        <input type="text" id="fname" name="fname" value=""><br>&emsp;
+        <label for="lname">Last name:</label><br>&emsp;
+        <input type="text" id="lname" name="lname" value=""><br>&emsp;
+        <label for="prn">PRN number</label><br>&emsp;
+        <input type="text" id="prn" name="prn" value=""><br>&emsp;
+        <label for="Year">Passing Batch Year</label><br>&emsp;
+        <input type="text" id="year" name="year" value=""><br><br>&emsp;
+        <input type="submit" value="Submit">&emsp;
+        </form> 
+
+        
+
+</body>
 </html>
 
 
