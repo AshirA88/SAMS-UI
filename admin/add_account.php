96e20256a5caf5c5429359c51445cb17542898b7
AshirA88 <duragkarashir@gmail.com>
2022-04-01 10:26:28 +0530

    Changed Validation

--- admin/add_account.php
+++ admin/add_account.php
@@ -45,24 +45,28 @@
     <title>Smart Attendance Management System Using Raspberry PI</title>
     <!-- VAlidation Javascript code-->
     <script>
-function validateForm() {
-  var x = document.forms["add_account"]["UserName"].value;
-  var y = document.forms["add_account"]["Password"].value;
-  var z = document.forms["add_account"]["UserType"].value;
-  if (x == "" || x == null) {
-    alert("Username must be filled out");
-    return false;
-  }
-  if (y == "" || y == null) {
-    alert("Password must be filled out");
-    return false;
-  }
-  if (z == "" || z == null) {
-    alert("UserType must be filled out");
-    return false;
-  }
-}
-</script>
+        function validateForm() 
+        {
+            var x = document.forms["add_account"]["UserName"].value;
+            var y = document.forms["add_account"]["Password"].value;
+            var z = document.forms["add_account"]["UserType"].value;
+            if (x == "" || x == null) 
+            {
+                alert("Username must be filled out");
+                return false;
+            }
+            if (y == "" || y == null) 
+            {
+                alert("Password must be filled out");
+                return false;
+            }
+            if (z == "" || z == null) 
+            {
+                alert("UserType must be filled out");
+                return false;
+            }
+        }
+    </script>
 <!-- VAlidation Javascript code-->
 </head>
 
@@ -81,9 +85,9 @@ function validateForm() {
                 }
         ?>
 
-        <form name="add_account" onsubmit="return validateForm()" method = "post" required>&emsp;
+        <form name="add_account" onsubmit="return validateForm()" method = "post">&emsp;
             <label for="UserName">UserName:<span style="color:red">*</span></label><br>&emsp;
-            <input type="text" id="UserName" name="UserName" placeholder="UserName"><br>&emsp;
+            <input type="text" id="UserName" name="UserName" placeholder="UserName" required pattern="[a-zA-Z0-9]+" title="UserName can only be Alphanumeric without spaces" required data-validation-required-message="Please enter UserName."><br>&emsp;
             <label for="Password">Password:<span style="color:red">*</span></label><br>&emsp;
             <input type="password" id="Password" name="Password" placeholder="Password"><br>&emsp;
             <label for="UserType">UserType"<span style="color:red">*</span></label><br>&emsp;
--- admin/manage_account.php
+++ admin/manage_account.php
@@ -84,9 +84,32 @@
                                 <td><?php echo htmlentities($result->UserType);?></td>      <!-- to print userType A,S,T -->
                                 <td><?php echo htmlentities($result->TimeOfUpdate);?></td>  <!-- to print timeOfCreation -->
                                 <td>
-                                    <form name="#" action="javascript:select();" >
-                                        <input id="edit" type="submit" name="edit" value="Edit" />
-                                    </form>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#edit_data">
+                                        Edit
+                                    </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="edit_data" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalCenterTitle">Edit Data</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    ...
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
                                     <form name="#" action="javascript:select();" >
                                         <input id="delete" type="submit" name="delete" value="Delete" />
                                     </form>
