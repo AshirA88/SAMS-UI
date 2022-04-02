a2cf132bc25fb3d0a71dff8636a5cc5a03208903
Itzrtune <rohitladdha26@gmail.com>
2022-04-02 12:32:59 +0530

    init

---
+++ teacherdash.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    error_reporting(0);
+    include('includes/config.php');
+    if(strlen($_SESSION['alogin'])==0)
+    {
+        header('location:index.php');
+    }
+    else
+    {
+        //main logic
+?>
