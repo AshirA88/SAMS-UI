5e6142a1385124fc691038381583db5fb52d3d05
AshirA88 <duragkarashir@gmail.com>
2022-04-12 22:19:36 +0530

    Minor changes and correction in some paths

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -20,6 +20,7 @@
     <head>
         <title>Smart Attendance Management System Using Raspberry PI</title>
         <link rel = "icon" href = "../img/logo.png" type = "image/x-icon">
+        <!-- Used for calender -->
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <!-- include jQuery UI -->
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -34,7 +35,8 @@
             width: 200px;
             height: 120px;      
         }
-        div
+        
+        #carousel_slide         /* Targetting id for carousel max height */
         {
             max-height: 700px
         }
@@ -44,33 +46,33 @@
     <body>
     
     <!-- Carousel -->
-    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+    <div id="carousel_slide" class="carousel slide" data-bs-ride="carousel">
 
     <!-- Indicators/dots -->
     <div class="carousel-indicators">
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+    <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="0" class="active"></button>
+    <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="1"></button>
+    <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="2"></button>
     </div>
 
     <!-- The slideshow/carousel -->
     <div class="carousel-inner">
     <div class="carousel-item active">
-        <img src="../img/carousel1.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+        <img src="../img/carousel1.jpg" class="d-block" style="width:100%">
     </div>
     <div class="carousel-item">
-        <img src="../img/carousel2.jpg" alt="Chicago" class="d-block" style="width:100%"> 
+        <img src="../img/carousel2.jpg" class="d-block" style="width:100%"> 
     </div>
     <div class="carousel-item">
-        <img src="../img/carousel3.jpg" alt="New York" class="d-block" style="width:100%">
+        <img src="../img/carousel3.jpg" class="d-block" style="width:100%">
     </div>
     </div>
 
     <!-- Left and right controls/icons -->
-    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel_slide" data-bs-slide="prev">
     <span class="carousel-control-prev-icon"></span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carousel_slide" data-bs-slide="next">
     <span class="carousel-control-next-icon"></span>
     </button>
     </div>
@@ -85,13 +87,12 @@
      }); 
     });
     </script>
-    <!--calendar ends -->
-    <h1><button><a href = "logout.php"> Logout </a>  </button>  </h1>                <!-- Logout to homepage -->
-    <h1><button><a href = "manage_account.php"> Management </a><button> </h1>       <!-- Redirect to manage_account.php from further CRUD operations -->
     </div>
+    <!--calendar ends -->
+
     <br><br><br><br><br>
     <div>
-    <img src="../img/download.jpg" alt="New York" width="100%" height="150">
+    <img src="../img/download.jpg" width="100%" height="150">
     </div>
     </body>
     
