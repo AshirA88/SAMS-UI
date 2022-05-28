e533f9a663bb3fec9d385ee88733f765e671ee14
AshirA88 <duragkarashir@gmail.com>
2022-05-28 18:17:26 +0530

    dashboard update

--- student/dashboard.php
+++ student/dashboard.php
@@ -23,7 +23,39 @@
     </head>
 
     <body>
-    <h1>Reached</h1>
+        <!-- Carousel -->
+        <div id="carousel_slide" class="carousel slide" data-bs-ride="carousel">
+
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#carousel_slide" data-bs-slide-to="2"></button>
+        </div>
+
+        <!-- The slideshow/carousel -->
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../img/carousel1.jpg" class="d-block" style="width:100%">
+        </div>
+        <div class="carousel-item">
+            <img src="../img/carousel2.jpg" class="d-block" style="width:100%"> 
+        </div>
+        <div class="carousel-item">
+            <img src="../img/carousel3.jpg" class="d-block" style="width:100%">
+        </div>
+        </div>
+
+        <!-- Left and right controls/icons -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel_slide" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel_slide" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+        </button>
+        </div>
+        <br>
+
     </body>
 </html>
 
